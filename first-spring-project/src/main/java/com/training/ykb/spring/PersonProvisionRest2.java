8aa14ad200fd7e783fe7d00c12da41bb26818ec0
Osman <osman.yaycioglu@gmail.com>
2020-06-30 11:16:40 +0300

    Day 2 3

--- first-spring-project/src/main/java/com/training/ykb/spring/PersonProvisionRest2.java
+++ first-spring-project/src/main/java/com/training/ykb/spring/PersonProvisionRest2.java
@@ -1,5 +1,8 @@
 package com.training.ykb.spring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,20 +14,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/person/v1")
 public class PersonProvisionRest2 {
 
+    private final Map<String, Person> personMap = new HashMap<>();
+
     @PostMapping("/provision")
     public void provision(@RequestBody final Person person) {
+        this.personMap.put(person.getName(),
+                           person);
     }
 
     @GetMapping("/deactivate")
-    public void deactivate(@RequestParam("id") final long personId) {
+    public void deactivate(@RequestParam("id") final String personName) {
+        this.personMap.remove(personName);
     }
 
     @PostMapping("/update")
     public void update(@RequestBody final Person person) {
+        this.personMap.put(person.getName(),
+                           person);
+
     }
 
     @GetMapping("/get")
-    public void get(@RequestParam("id") final long personId) {
+    public Person get(@RequestParam("id") final String personId) {
+        return this.personMap.get(personId);
     }
 
 
